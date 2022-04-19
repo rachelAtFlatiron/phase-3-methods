49627150702588ffcc6e35b76cd50f8410489391
Rachel Katz <rachel.katz@flatironschool.com>
2022-04-19 14:13:55 -0400

    my solution

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(a,b)
+    a + b
+end
+
+def halve(a = -1)
+    return nil unless a.class==Integer
+    a/2 
+end
